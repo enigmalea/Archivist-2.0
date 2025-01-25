c861a6c5db050250220686d509f3fe8a286e8d4a
enigmalea <73132777+enigmalea@users.noreply.github.com>
2025-01-25 16:37:24 -0500

    adds list command. (#34)

--- utils/errors.ts
+++ utils/errors.ts
@@ -1,6 +1,6 @@
 import { oneLine } from "common-tags";
 
-// Constructs and exports the error message used when the link provided is not for an AO3Work.
+// Constructs and exports the error message used when the link provided is not for an AO3 Work.
 const ao3WorkMessage = oneLine`That does not appear to be a link to an AO3 work.
 	Please make sure you are linking to a work and not a series, author, collection,
 	or using a non-AO3 link.`;
@@ -9,7 +9,16 @@ export const ao3WorkError = {
   ephemeral: true,
 };
 
-// Constructs and exports the error message used with the user must be authenticated to see a work.
+// Constructs and exports the error message used when the link provided is not for an AO3 Series.
+const ao3SeriesMessage = oneLine`That does not appear to be a link to an AO3 series.
+	Please make sure you are linking to a series and not a work, author, collection,
+	or using a non-AO3 link.`;
+export const ao3SeriesError = {
+  content: ao3SeriesMessage,
+  ephemeral: true,
+};
+
+// Constructs and exports the error message used with the user must be authenticated to see an AO3 Work.
 const authMessage = oneLine`I'm sorry. This fic is available to Registered
 	Users of AO3 only. In order to protect the author's privacy, I will not display
 	an embed. Please go to AO3 directly while logged in to interact with this fic!`;
